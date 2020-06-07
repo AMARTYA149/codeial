39130db3a8d8f87766fd311af7c5ebb2f73f3b74
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-07 17:07:04 +0530

    user controller added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ const homeController = require('../controllers/home_controller');
 console.log("Router Loaded");
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+//for any further routes, access from here
+// router.use('/routerName', require('./routerfile'));
 module.exports = router;
