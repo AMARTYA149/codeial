c937e28406a2d0873975985e10e6397a9711263d
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-13 11:24:12 +0530

    sign up creation

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 module.exports.home = function(request, response){
     // return response.end('<h1>Express is up for Codeial!</h1>');
+   // console.log(request.cookies);
     return response.render('home', {
         title: "Home"
     });
