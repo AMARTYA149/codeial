59249255d44569c3a02cfe6e60bce3a32db712ce
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-18 16:37:47 +0530

    showing post's username

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(request, response){
     // return response.end('<h1>Express is up for Codeial!</h1>');
    // console.log(request.cookies);
-    return response.render('home', {
-        title: "Home"
-    });
+   //res.cookie('user_id', 25);
+//    Post.find({}, function(err, posts){
+//     return response.render('home', {
+//         title: "Codeial | Home",
+//         posts: posts
+//     });
+//    });    
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return response.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+       });
 }
 
 // module.exports.actionName = function(request, response){}
