da9bc9301543737718ba424c7a1e42a8503a0e93
Amartya Aishwarya <amartya970914@gmail.com>
2020-06-22 22:43:22 +0530

    deletion of comment by post's owner

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -23,17 +23,36 @@ module.exports.create = function(request, response){
 
 module.exports.destroy = function(request, response){
     Comment.findById(request.params.id, function(err, comment){
+        let postId = comment.post;
+        
+        //Deletion of comment by Comment's Owner
         if(comment.user == request.user.id){
-
-            let postId = comment.post;
-
+            
             comment.remove();
 
             Post.findByIdAndUpdate(postId, { $pull: {comments: request.params.id}}, function(err, post){
                 return response.redirect('back');
             })
-        } else {
-            return response.redirect('back');
+        }  else {
+
+            //Deletion of comment by Post's Owner (Might need rectification)
+            Post.findById(postId, function(err, post){
+                if(post.user == request.user.id){
+            
+                    // let postId = comment.post;
+        
+                    comment.remove();
+        
+                    Post.findByIdAndUpdate(postId, { $pull: {comments: request.params.id}}, function(err, post){
+                        return response.redirect('back');
+                    });
+                }
+                else {
+                    return response.redirect('back');
+                } 
+            });
+
+            
         }
     })
 }
