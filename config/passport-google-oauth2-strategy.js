d1e95fc649aac2f340a88d850784d9fb82fc4ead
Amartya Aishwarya <amartya970914@gmail.com>
2020-07-12 16:39:50 +0530

    signin/signup using google

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const User = require('../models/user');
 
 
+//tell passport to use a new strategy for google login
 passport.use(new googleStrategy({
         clientID: "350982341333-2u0c8jjachg7nk3ha485ojbkq6bo1bhu.apps.googleusercontent.com" ,
         clientSecret: "x6g6KIegQ8C_vgjevMqSrOVB",
@@ -11,6 +12,36 @@ passport.use(new googleStrategy({
     },
 
     function(accessToken, refreshToken, profile, done){
-        
+        //find a user
+        User.findOne({email: profile.emails[0].value}).exec(function(err, user){
+            if(err){
+                console.log('error in google strategy-passport', err);
+                return;
+            }
+
+            console.log(accessToken, refreshToken);
+            console.log(profile);
+
+            if(user){
+                //if found, set this user as req.user
+                return done(null, user);
+            } else {
+                //if not found, create the user and set it as req.user
+                User.create({
+                    name: profile.displayName,
+                    email: profile.emails[0].value,
+                    password: crypto.randomBytes(20).toString('hex')
+                }, function(err, user){
+                    if(err){
+                        console.log('error in creating user google strategy-passport', err);
+                        return;
+                    }
+
+                    return done(null, user);
+                });
+            }
+        });
     }
-))
+));
+
+module.exports = passport;
