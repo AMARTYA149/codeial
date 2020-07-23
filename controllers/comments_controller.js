0ba3bf5407007e6d439ef5f69f652d6091964af5
Amartya Aishwarya <amartya970914@gmail.com>
2020-07-23 17:09:57 +0530

    sending email via smtp

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-
+const commentsMailer = require('../mailers/comments_mailer');
 module.exports.create = async function(req, res){
 
     try{
@@ -15,10 +15,11 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
-
+            
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
             if (req.xhr){
-                // Similar for comments to fetch the user's id!
-                comment = await comment.populate('user', 'name').execPopulate();
+                
     
                 return res.status(200).json({
                     data: {
@@ -28,7 +29,9 @@ module.exports.create = async function(req, res){
                 });
             }
 
+
             req.flash('success', 'Comment published!');
+
             res.redirect('/');
         }
     }catch(err){
@@ -44,11 +47,9 @@ module.exports.destroy = async function(req, res){
     try{
         let comment = await Comment.findById(req.params.id);
 
-        let postId = comment.post;
-        let post = await Post.findById(postId);
+        if (comment.user == req.user.id){
 
-        //Deleting the comment only by Comment's owner or Post's owner
-        if (comment.user == req.user.id || post.user == req.user.id){
+            let postId = comment.post;
 
             comment.remove();
 
@@ -64,16 +65,17 @@ module.exports.destroy = async function(req, res){
                 });
             }
 
+
             req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
-
-        }else{  
-
+        }else{
             req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-    } catch(err) {
+    }catch(err){
         req.flash('error', err);
         return;
-    }    
+    }
+    
 }
